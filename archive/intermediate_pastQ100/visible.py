54d1af3b7cdbd49f4f74bbefaca44fd33949b631
klyn0101 <kobiyama-yuki-vn@ynu.jp>
2023-04-19 13:06:14 +0900

    4/19

--- archive/intermediate_pastQ100/visible.py
+++ archive/intermediate_pastQ100/visible.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 # ディレクトリを指定
 directory = "C:/programming/atcoder/archive/intermediate_pastQ100"
@@ -9,10 +10,11 @@ flags = [False] * 100
 # ディレクトリ内のファイル名を取得
 for filename in os.listdir(directory):
     try:
-        number, ext = os.path.splitext(filename)
-        number = int(number)
-        if 1 <= number <= 100 and ext == '.cpp':
-            flags[number - 1] = True
+        match = re.match(r"(\d+).*\.cpp", filename)
+        if match:
+            number = int(match.group(1))
+            if 1 <= number <= 100:
+                flags[number - 1] = True
     except ValueError:
         continue
 
@@ -23,9 +25,10 @@ with open("README.md", "w", encoding='utf-8') as f:
     f.write("|   #   | Status |   #   | Status |   #   | Status |   #   | Status |   #   | Status |\n")
     f.write("|:-----:|:------:|:-----:|:------:|:-----:|:------:|:-----:|:------:|:-----:|:------:|\n")
     
-    for i, flag in enumerate(flags, 1):
-        if i % 10 == 1:
-            f.write("|")
-        f.write(f" {i:3} | {'✓' if flag else ' '} |")
-        if i % 10 == 0:
-            f.write("\n")
+    for i in range(0, 100, 10):
+        for j in range(1, 11):
+            index = i + j
+            if j == 1:
+                f.write("|")
+            f.write(f" {index:3} | {'✓' if flags[index - 1] else ' '} |")
+        f.write("\n")
