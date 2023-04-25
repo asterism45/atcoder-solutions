cab299ec5765f2610665064674a34911a8206f95
Yuki K <kobiyama-yuki-vn@ynu.jp>
2023-04-25 17:38:05 +0900

    4/25

--- Atcoder/onlinejudge/cptest.sh
+++ Atcoder/onlinejudge/cptest.sh
@@ -2,8 +2,9 @@
 
 problem_name=$1
 problem_name=${problem_name##*/}
-test_dir=archive/onlinejudge/test/${problem_name}
-base_url=$(echo ${problem_name} | rev | cut -d'_' -f2- | rev | cut -d'/' -f2)
+problem_name=${problem_name//Atcoder\\ABC\\/}
+test_dir=AtCoder/onlinejudge/test/${problem_name}
+base_url=${problem_name%_*}
 code_path=$2
 
 # make test directory
@@ -11,4 +12,7 @@ if [ ! -e ${test_dir} ]; then
     oj dl -d ${test_dir} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
 fi
 
-g++ -std=gnu++17 -Wall -Wextra -O2 ${code_path} && oj test -c "./a.out " -d ${test_dir}
+g++ -std=gnu++17 -Wall -Wextra -O2 ${code_path} -o a.out && oj test -c "a.out" -d ${test_dir}
+
+echo "Press ENTER to exit..."
+read
