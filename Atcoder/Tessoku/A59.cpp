3a5ad606825ea09930aa73273812c53dfc7e0b1c
klyn0101 <kobiyama-yuki-vn@ynu.jp>
2023-05-10 06:28:24 +0900

    5/10

--- Atcoder/Tessoku/A59.cpp
+++ Atcoder/Tessoku/A59.cpp
@@ -22,27 +22,25 @@ void outp(bool flag) { cout << (flag ? "Yes" : "No") << endl; }
 
 class SegmentTree {
 public:
-    ll siz = 1;
     vll dat;
+    ll siz = 1;
     SegmentTree(ll N) {
-        while (siz < N) siz *= 2;
+        while (N > siz) siz *= 2;
         dat.resize(2 * siz);
-        rep(i, 2 * siz) dat[i] = 0;
     }
     void update(ll pos, ll x) {
         ll num = pos + siz - 1;
         dat[num] = x;
         while ((num /= 2) != 0)
-            dat[num] = max(dat[2 * num], dat[2 * num + 1]);
+            dat[num] = dat[2 * num] + dat[2 * num + 1];
     }
     ll query(ll l, ll r, ll a, ll b, ll u) {
         if (b <= l || r <= a)
-            return -INF;
-        else if (l <= a && b <= r)
+            return 0;
+        if (l <= a && b <= r)
             return dat[u];
-
         ll m = (a + b) / 2;
-        return max(query(l, r, a, m, 2 * u), query(l, r, m, b, 2 * u + 1));
+        return query(l, r, a, m, 2 * u) + query(l, r, m, b, 2 * u + 1);
     }
 };
 
@@ -59,9 +57,8 @@ int main()
             st.update(pos, x);
         }
         else {
-            ll l, r;
-            cin >> l >> r;
-            cout << st.query(l, r, 1, st.siz + 1, 1) << endl;
+            cin >> L >> R;
+            cout << st.query(L, R, 1, st.siz + 1, 1) << endl;
         }
     }
 
